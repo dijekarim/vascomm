c3e5c47166e888ea57dad19f2cba3744c8f521a1
dimas jk <dijekarim@gmail.com>
2022-12-23 17:58:48 +0700

    initial commit

---
+++ config/db.js
@@ -0,0 +1,13 @@
+import mysql from "serverless-mysql";
+
+const pool = mysql({
+  config: {
+    host: "127.0.0.1",
+    user: "root",
+    password: "password",
+    port: 3307,
+    database: "vascomm-dev",
+  },
+});
+
+export { pool };
